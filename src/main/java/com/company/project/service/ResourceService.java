af6f5066436d128c4ba05d95e68fc427776cdd30
yefei <2360274407@qq.com>
2019-08-20 17:09:49 +0800

    资源curd

--- src/main/java/com/company/project/service/ResourceService.java
+++ src/main/java/com/company/project/service/ResourceService.java
@@ -2,11 +2,22 @@ package com.company.project.service;
 
 import com.company.project.dao.ResourceMapper;
 import com.company.project.model.Resource;
+import com.company.project.model.Resource;
 import com.company.project.service.ResourceService;
+import com.company.project.util.Conditions;
+import com.company.project.util.Utils;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.company.project.core.AbstractService;
+import com.company.project.core.Result;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -18,5 +29,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class ResourceService extends AbstractService<Resource> {
     @Autowired
     private ResourceMapper resourceMapper;
+    
+    public Result list(String name,Page page) {
+    	PageHelper.startPage(page.getPageNum(),page.getPageSize());
+    	Conditions con=Conditions.instance(Resource.class);
+    	con.notDeleted();
+    	if(StringUtils.isNotEmpty(name)) {
+    		con.firstCriteria().andLike("name", "%"+name+"%");
+    	}
+    	List<Resource> list=this.findByCondition(con);
+    	PageInfo pageResult = new PageInfo(list);
+    	return Utils.success(pageResult);
+    }
+    
+    public Result saveOrUpdateUser(Resource resource) {
+    	if(resource.getId()==null) {
+    		resource.setDeleted(false);
+    	}
+    	this.saveOrUpdate(resource);
+    	return Utils.success();
+    }
 
 }
--- src/main/java/com/company/project/web/ResourceController.java
+++ src/main/java/com/company/project/web/ResourceController.java
@@ -1,12 +1,15 @@
 package com.company.project.web;
-import com.company.project.core.Result;
-import com.company.project.core.ResultGenerator;
-import com.company.project.core.AbstractController;
-import com.company.project.service.ResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import com.company.project.core.AbstractController;
+import com.company.project.core.Result;
+import com.company.project.model.Resource;
+import com.company.project.service.ResourceService;
+import com.company.project.util.Utils;
 
 /**
 * Created by CodeGenerator on 2019/08/10.
@@ -17,9 +20,25 @@ public class ResourceController extends AbstractController{
     @Autowired
     private ResourceService resourceService;
 
-    @GetMapping("/test")
-    public Result test() {
-        return ResultGenerator.genSuccessResult(resourceService.findAll());
+    @GetMapping("/list")
+    public Result userList(String name) {
+    	return resourceService.list(name, getRequestPage());
+    }
+    
+    @PostMapping("/save")
+    public Result saveOrUpdate(Resource resource) {
+    	return resourceService.saveOrUpdateUser(resource);
+    }
+    
+    @GetMapping("/info")
+    public Result info(Integer id) {
+    	return Utils.success(resourceService.findById(id));
+    }
+
+    @PostMapping("/delete")
+    public Result delete(Integer id) {
+    	resourceService.logicDeleteById(id);
+    	return Utils.success();
     }
 
 }
