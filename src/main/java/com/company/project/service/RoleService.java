f7f2221a082a943e8086b4dfd6578add0d0e95d3
yefei <2360274407@qq.com>
2019-08-20 16:42:01 +0800

    角色管理curd

--- src/main/java/com/company/project/service/RoleService.java
+++ src/main/java/com/company/project/service/RoleService.java
@@ -1,13 +1,23 @@
 package com.company.project.service;
 
-import com.company.project.dao.RoleMapper;
-import com.company.project.model.Role;
-import com.company.project.service.RoleService;
-import com.company.project.core.AbstractService;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.company.project.core.AbstractService;
+import com.company.project.core.Result;
+import com.company.project.dao.RoleMapper;
+import com.company.project.model.Role;
+import com.company.project.model.User;
+import com.company.project.util.Conditions;
+import com.company.project.util.Utils;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 
 /**
@@ -16,7 +26,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Service
 @Transactional
 public class RoleService extends AbstractService<Role> {
+	
     @Autowired
     private RoleMapper roleMapper;
 
+    public Result list(String name,Page page) {
+    	PageHelper.startPage(page.getPageNum(),page.getPageSize());
+    	Conditions con=Conditions.instance(Role.class);
+    	con.notDeleted();
+    	if(StringUtils.isNotEmpty(name)) {
+    		con.firstCriteria().andLike("roledesc", "%"+name+"%");
+    	}
+    	List<Role> list=this.findByCondition(con);
+    	PageInfo pageResult = new PageInfo(list);
+    	return Utils.success(pageResult);
+    }
+    
+    public Result saveOrUpdateUser(Role role) {
+    	Role roleName=this.findByRoleName(role.getName());
+    	if(roleName!=null&&!roleName.getId().equals(role.getId())) {
+    		Utils.ServiceException("角色名不能重复");
+    	}
+    	if(role.getId()==null) {
+    		role.setDeleted(false);
+    	}
+    	this.saveOrUpdate(role);
+    	return Utils.success();
+    }
+    
+    public Role findByRoleName(String name) {
+    	Conditions con=Conditions.instance(User.class);
+    	con.notDeleted().andEqualTo("name", name);
+    	Role role=this.findOneByCondition(con);
+    	return role;
+    }
+    
 }
--- src/main/java/com/company/project/web/RoleController.java
+++ src/main/java/com/company/project/web/RoleController.java
@@ -1,12 +1,15 @@
 package com.company.project.web;
-import com.company.project.core.Result;
-import com.company.project.core.ResultGenerator;
-import com.company.project.core.AbstractController;
-import com.company.project.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import com.company.project.core.AbstractController;
+import com.company.project.core.Result;
+import com.company.project.model.Role;
+import com.company.project.service.RoleService;
+import com.company.project.util.Utils;
 
 /**
 * Created by CodeGenerator on 2019/08/10.
@@ -17,9 +20,25 @@ public class RoleController extends AbstractController{
     @Autowired
     private RoleService roleService;
 
-    @GetMapping("/test")
-    public Result test() {
-        return ResultGenerator.genSuccessResult(roleService.findAll());
+    @GetMapping("/list")
+    public Result userList(String name) {
+    	return roleService.list(name, getRequestPage());
+    }
+    
+    @PostMapping("/save")
+    public Result saveOrUpdate(Role role) {
+    	return roleService.saveOrUpdateUser(role);
+    }
+    
+    @GetMapping("/info")
+    public Result info(Integer id) {
+    	return Utils.success(roleService.findById(id));
+    }
+
+    @PostMapping("/delete")
+    public Result delete(Integer id) {
+    	roleService.logicDeleteById(id);
+    	return Utils.success();
     }
 
 }
